a10939913ae3407e5fb5d4e44bc9286d4525bb1a
周美华 <sgffzwq@163.com>
2019-07-14 13:44:07 +0800

    配置

---
+++ spring-cloud-config/src/main/java/com/example/springcloudconfig/controller/EchoEnvController.java
@@ -0,0 +1,10 @@
+package com.example.springcloudconfig.controller;
+
+/**
+ * @author ：周美华
+ * @date ：Created in 2019/7/13 20:56
+ * @description：
+ * @modified By：
+ */
+public class EchoEnvController {
+}
